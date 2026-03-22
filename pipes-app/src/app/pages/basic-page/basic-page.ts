d32134f8bdb3ef7ab62f1edf121f9ee57025ab2b
Avila-Pato <p.avilaf1998@gmail.com>
2026-03-22 16:24:14 -0300

    feat: update basic and numbers pages with Angular pipes and improved layout

--- pipes-app/src/app/pages/basic-page/basic-page.ts
+++ pipes-app/src/app/pages/basic-page/basic-page.ts
@@ -1,9 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
+import { CommonModule, DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default  class BasicPage { }
+export default class BasicPage {
+
+  nameLower = signal('ironman')
+  nameUpper = signal('IRONMAN')
+  fullName = signal('Iván Díaz')
+
+
+  customDate = signal(new Date())
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date())
+    }, 1000)
+
+    onCleanup(() => {
+      clearInterval(interval)
+    })
+  })
+}
--- pipes-app/src/app/pages/numbers-page/numbers-page.ts
+++ pipes-app/src/app/pages/numbers-page/numbers-page.ts
@@ -1,9 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './numbers-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class NumbersPage { }
+export default class NumbersPage {
+
+  totalSells = signal(2_324_234.435345);
+  percent = signal(0.4856);
+}
